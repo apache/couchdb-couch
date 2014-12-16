825ac4420691026e1959b7b38c5abfcdfec7045b
ILYA Khlopotov <iilyak@ca.ibm.com>
2014-12-16 12:42:28 -0800

    Use namespace argument to select correct tree for the enum_docs request
    COUCHDB-1145

--- src/couch_db.erl
+++ src/couch_db.erl
@@ -1210,12 +1210,27 @@ enum_docs_since(Db, SinceSeq, InFun, Acc, Options) ->
             [{start_key, SinceSeq + 1} | Options]),
     {ok, enum_docs_since_reduce_to_count(LastReduction), AccOut}.
 
-enum_docs(Db, InFun, InAcc, Options) ->
+enum_docs(Db, InFun, InAcc, Options0) ->
+    {NS, Options} = extract_namespace(Options0),
     FoldFun = skip_deleted(InFun),
-    {ok, LastReduce, OutAcc} = couch_btree:fold(
-        Db#db.id_tree, FoldFun, InAcc, Options),
-    {ok, enum_docs_reduce_to_count(LastReduce), OutAcc}.
+    case NS of
+        all ->
+            {ok, LastReduce, OutAcc} = couch_btree:fold(
+                Db#db.id_tree, FoldFun, InAcc, Options),
+            {ok, enum_docs_reduce_to_count(LastReduce), OutAcc};
+        local ->
+            {ok, _LastReduce, OutAcc} = couch_btree:fold(
+                Db#db.local_tree, FoldFun, InAcc, Options),
+            {ok, 0, OutAcc}
+    end.
 
+extract_namespace(Options0) ->
+    case proplists:split(Options0, [namespace]) of
+        {[[{namespace, NS}]], Options} ->
+            {NS, Options};
+        {_, Options} ->
+            {all, Options}
+    end.
 
 %%% Internal function %%%
 open_doc_revs_int(Db, IdRevs, Options) ->
